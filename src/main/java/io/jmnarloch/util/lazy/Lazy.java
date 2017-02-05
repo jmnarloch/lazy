9fb2c10376c07c96f958234e6ff2615abf47652a
Jakub Narloch <jmnarloch@gmail.com>
2017-02-04 21:44:25 -0800

    Lock free Lazy implementation

--- src/main/java/io/jmnarloch/util/lazy/Lazy.java
+++ src/main/java/io/jmnarloch/util/lazy/Lazy.java
@@ -1,7 +1,116 @@
+/**
+ * Copyright (c) 2015-2017 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jmnarloch.util.lazy;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  *
  */
-public interface Lazy {
+public interface Lazy<T> {
+
+    T get();
+
+    default <U> Lazy<U> map(Function<T, U> map) {
+        return create(() -> map.apply(get()));
+    }
+
+    default <U> Lazy<U> flatMap(Function<T, Lazy<U>> map) {
+        return create(() -> map.apply(get()).get());
+    }
+
+    static <T> Lazy<T> create(Supplier<T> supplier) {
+        return new LockFreeLazy<>(supplier);
+    }
+
+    class LockFreeLazy<T> implements Lazy<T> {
+
+        private static final Object INITIALIZING = new Object();
+
+        private static final Object ERROR = new Object();
+
+        private final Supplier<T> supplier;
+
+        private final AtomicReference instance = new AtomicReference();
+
+        LockFreeLazy(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public T get() {
+            Object value = getValue();
+            checkError(value);
+            if (isValueSet(value)) {
+                return (T) value;
+            }
+
+            if (trySetValue()) {
+                setValue();
+            }
+
+            value = waitForValue();
+            checkError(value);
+            return (T) value;
+        }
+
+        private boolean isValueSet(Object value) {
+            return value != null && value != INITIALIZING && value != ERROR;
+        }
+
+        @SuppressWarnings("unchecked")
+        private boolean trySetValue() {
+            return instance.compareAndSet(null, INITIALIZING);
+        }
+
+        @SuppressWarnings("unchecked")
+        private void setValue() {
+            try {
+                T newValue = supplier.get();
+                if (Objects.isNull(newValue)) {
+                    throw new IllegalStateException("Supplier failed to provide the value");
+                }
+                instance.set(newValue);
+            } catch (Exception ex) {
+                instance.set(ERROR);
+                throw new IllegalStateException("Failed to initialize the lazy instance", ex);
+            }
+        }
+
+        private Object getValue() {
+            return instance.get();
+        }
+
+        private Object waitForValue() {
+            Object value;
+            do {
+                value = getValue();
+            } while (value == INITIALIZING);
+            return value;
+
+        }
+
+        private void checkError(Object value) {
+            if (value == ERROR) {
+                throw new IllegalStateException("Supplier failed to initialize the value");
+            }
+        }
+    }
 }
--- src/test/java/io/jmnarloch/util/lazy/LazyTest.java
+++ src/test/java/io/jmnarloch/util/lazy/LazyTest.java
@@ -1,4 +1,22 @@
+/**
+ * Copyright (c) 2015-2017 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jmnarloch.util.lazy;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -7,4 +25,151 @@ import static org.junit.Assert.*;
  */
 public class LazyTest {
 
+    @Before
+    public void setUp() throws Exception {
+
+        SimplePojo.resetInstanceCount();
+        EmbeddedLazy.resetInstanceCount();
+    }
+
+    @Test
+    public void shouldCreateLazyValue() {
+
+        // given
+        final Lazy<String> value = Lazy.create(() -> "Value");
+
+        // when
+        final String result = value.get();
+
+        // then
+        assertNotNull(result);
+        assertEquals("Value", result);
+    }
+
+    @Test
+    public void shouldEvaluateValueOnlyOnce() {
+
+        // given
+        final Lazy<SimplePojo> value = Lazy.create(() -> new SimplePojo("Value"));
+
+        // when
+        value.get();
+        // and
+        value.get();
+
+        // then
+        assertEquals(1, SimplePojo.getInstanceCount());
+    }
+
+    @Test
+    public void shouldMapLazyValue() {
+
+        // given
+        final Lazy<SimplePojo> value = Lazy.create(() -> new SimplePojo("Value"));
+
+        // when
+        final Lazy<String> result = value.map(SimplePojo::getValue);
+
+        // then
+        assertNotNull(result);
+        assertEquals("Value", result.get());
+    }
+
+    @Test
+    public void shouldMapLazyValueWithLazyEvaluation() {
+
+        // given
+        final Lazy<SimplePojo> value = Lazy.create(() -> new SimplePojo("Value"));
+
+        // when
+        final Lazy<String> result = value.map(SimplePojo::getValue);
+
+        // then
+        assertNotNull(result);
+        assertEquals(0, SimplePojo.getInstanceCount());
+        assertEquals("Value", result.get());
+        assertEquals(1, SimplePojo.getInstanceCount());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldFailToInitializeOnMappingNullValue() {
+
+        // given
+        final Lazy<SimplePojo> value = Lazy.create(() -> new SimplePojo(null));
+
+        // then
+        value.map(SimplePojo::getValue).get();
+    }
+
+    @Test
+    public void shouldFlatMapLazyValueWithLazyEvaluation() {
+
+        // given
+        final Lazy<EmbeddedLazy<String>> value = Lazy.create(() -> new EmbeddedLazy<>(Lazy.create(() -> "Value")));
+
+        // when
+        final Lazy<String> result = value.flatMap(EmbeddedLazy::getValue);
+
+        // then
+        assertNotNull(result);
+        assertEquals(0, EmbeddedLazy.getInstanceCount());
+    }
+
+    @Test
+    public void shouldFlatMapLazyValue() {
+
+        // given
+        final Lazy<EmbeddedLazy<String>> value = Lazy.create(() -> new EmbeddedLazy<>(Lazy.create(() -> "Value")));
+
+        // when
+        final Lazy<String> result = value.flatMap(EmbeddedLazy::getValue);
+
+        // then
+        assertNotNull(result);
+        assertEquals(0, EmbeddedLazy.getInstanceCount());
+        assertEquals("Value", result.get());
+        assertEquals(1, EmbeddedLazy.getInstanceCount());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldThrowExceptionOnNullValue() {
+
+        // given
+        final Lazy<Object> value = Lazy.create(() -> null);
+
+        // then
+        value.get();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldThrowExceptionOnSupplierError() {
+
+        // given
+        final Lazy<Object> value = Lazy.create(() -> {
+            throw new RuntimeException();
+        });
+
+        // then
+        value.get();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldThrowExceptionOnConsecutiveCallsWhenFailed() {
+
+        // given
+        final Lazy<Object> value = Lazy.create(() -> {
+            throw new RuntimeException();
+        });
+
+        // and
+        try {
+            value.get();
+            fail();
+        } catch (IllegalStateException e) {
+            // should fail
+        }
+
+        // then
+        value.get();
+    }
 }
